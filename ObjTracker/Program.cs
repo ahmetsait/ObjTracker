310506103c49798c783aff1e845b71f64a8124fe
Ahmet Sait <ahmetsaitkocak@gmail.com>
2019-01-14 14:28:05 +0300

    Automatic port finding
    + Faster max contour algorithm
    + Reformatting

--- ObjTracker/Program.cs
+++ ObjTracker/Program.cs
@@ -2,49 +2,47 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO;
 using System.IO.Ports;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Linq;
+
 using OpenCvSharp;
 
 namespace ObjTracker
 {
-    internal static class Program
-    {
 
-        static int Map(this int x, int in_min, int in_max, int out_min, int out_max)
-        {
-            return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-        }
+	internal static class Program
+	{
+		static readonly Scalar greenLower = new Scalar(29, 86, 6);
+		static readonly Scalar greenUpper = new Scalar(64, 255, 255);
 
-        static Scalar FindBallCoordinates(int ballRadiusInPixel, Point centerOfBall, Point CENTER_OF_SCREEN, float BALL_DIAMETER = 6.5f)
-        {
-            //finding ball distance in cantimeters from camera with precalculated constants
-            double ballDist = 60 * 20 / (ballRadiusInPixel - 1);
-            //relative coordinates according to camera
-            double x = (centerOfBall.X - CENTER_OF_SCREEN.X) * BALL_DIAMETER / (ballRadiusInPixel - 1) / 2;
-            double y = (centerOfBall.Y - CENTER_OF_SCREEN.Y) * BALL_DIAMETER / (ballRadiusInPixel - 1) / 2;
-            double z = Math.Sqrt((Math.Pow(ballDist, 2) - Math.Pow(x, 2))) * 2;
-            return new Scalar(x, y, z);
-        }
+		const string serialDevice = "Arduino Leonardo";
+		const int baudRate = 115200;
 
-        public static int Main(string[] args)
+		public static int Main(string[] args)
 		{
-            Scalar lastSeen = new Scalar();
-            Scalar greenLower = new Scalar(29, 86, 6);
-            Scalar greenUpper = new Scalar(64, 255, 255);
-            const string PORT = "COM5";
-            const int BAUD_RATE = 115200;
-            Console.WriteLine("Connecting Serial Port");
-            SerialPort serial = new SerialPort(PORT, BAUD_RATE);
-            serial.Open();
-            Console.WriteLine("Connected " + PORT + " with " + BAUD_RATE + " baud rate");
-            TcpListener listener = new TcpListener(IPAddress.Any, 5050);
+			SerialPort serial;
+			try
+			{
+				IEnumerable<PortInfo> ports = PortInfo.GetPorts();
+				string port = ports.FirstOrDefault((pi) => pi.Description == serialDevice)?.DeviceID;
+				if (port != null)
+					serial = new SerialPort(port, baudRate);
+				else
+					throw new IOException("Could not connect to serial port.");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				return 1;
+			}
+
+			Console.WriteLine();
+			TcpListener listener = new TcpListener(IPAddress.Any, 5050);
 			re:
 			try
 			{
@@ -77,121 +75,99 @@ namespace ObjTracker
 								{
 									Console.WriteLine("Started video capturing...");
 									Mat imgMatrix = new Mat();
-                                    Mat mask = new Mat();
-                                    Mat tresh = new Mat();
+									Mat mask = new Mat();
+									Mat tresh = new Mat();
 									try
 									{
+										Scalar lastSeen = default(Scalar);
 										while (client.Connected && stream.CanWrite)
 										{
 											if (cap.Read(imgMatrix))
 											{
+												Cv2.CvtColor(imgMatrix, mask, ColorConversionCodes.BGR2HSV);
+												Cv2.InRange(mask, greenLower, greenUpper, tresh);
+												Cv2.Erode(tresh, tresh, null, iterations: 2);
+												Cv2.Dilate(tresh, tresh, null, iterations: 2);
 
-                                                Cv2.CvtColor(imgMatrix, mask, ColorConversionCodes.BGR2HSV);
+												Cv2.FindContours(
+													tresh,
+													out Point[][] contours,
+													out HierarchyIndex[] hierarchyIndexes,
+													RetrievalModes.External,
+													ContourApproximationModes.ApproxSimple
+													);
 
-                                                Cv2.InRange(mask, greenLower, greenUpper, tresh);
+												if (contours.Length > 0)
+												{
+													contours.OrderBy(element => Cv2.ContourArea(element));
+													Point[] max = contours[contours.Length - 1];
 
-                                                Cv2.Erode(tresh, tresh, null, iterations: 2);
+													Cv2.MinEnclosingCircle(max, out Point2f xy, out float radius);
 
-                                                Cv2.Dilate(tresh, tresh, null, iterations: 2);
+													//Moments M = Cv2.Moments(max);
+													//Point center = new Point((M.M10 / M.M00), (M.M01 / M.M00));
 
-                                                Cv2.FindContours(
-                                                    tresh,
-                                                    out Point[][] contours,
-                                                    out HierarchyIndex[] hierarchyIndexes,
-                                                    RetrievalModes.External,
-                                                    ContourApproximationModes.ApproxSimple
-                                                    );
+													Point center = new Point(xy.X, xy.Y);
 
-                                                
+													if (radius > 10.0f)
+													{
+														Cv2.Circle(imgMatrix, center, (int)radius, new Scalar(0, 255, 255), thickness: 2);
+														Cv2.Circle(imgMatrix, center, 5, new Scalar(0, 0, 255), thickness: -1);
 
-                                                if (contours.Length > 0)
-                                                {
-                                                    contours.OrderBy(element => Cv2.ContourArea(element));
-                                                    Point[] max = contours[contours.Length - 1];
+														//find the ball which region on the screen horizontally [0-5]
+														int xRegion = center.X.Map(0, width, 0, 3);
+														//find the ball which region on the screen vertically [0-5]
+														int yRegion = center.Y.Map(0, height, 0, 3);
+														//find the ball is too far from camera or too close
+														int zRegion = ((int)radius).Map(10, 400, 0, 3);
 
-                                                    Cv2.MinEnclosingCircle(max, out Point2f xy, out float radius);
+														lastSeen = FindBallCoordinates((int)radius, center, new Point(width / 2, height / 2));
 
-                                                    //Moments M = Cv2.Moments(max);
-                                                    //Point center = new Point((M.M10 / M.M00), (M.M01 / M.M00));
+														#region Automatic Decisions
 
+														byte command = 0;
 
-                                                    Point center = new Point(xy.X, xy.Y);
+														if (xRegion < 1 && yRegion < 1)
+															command |= (byte)(Command.CamLeft | Command.CamUp);
+														else if (xRegion < 1 && yRegion > 1)
+															command |= (byte)(Command.CamLeft | Command.CamDown);
+														else if (xRegion > 1 && yRegion < 1)
+															command |= (byte)(Command.CamRight | Command.CamUp);
+														else if (xRegion > 1 && yRegion > 1)
+															command |= (byte)(Command.CamRight | Command.CamDown);
+														else if (xRegion < 1)
+															command |= (byte)Command.CamLeft;
+														else if (xRegion > 1)
+															command |= (byte)Command.CamRight;
+														else if (yRegion < 1)
+															command |= (byte)Command.CamUp;
+														else if (yRegion > 1)
+															command |= (byte)Command.CamDown;
+														if (zRegion > 1)
+															command |= (byte)Command.MoveBackward;
+														else if (zRegion < 1)
+															command |= (byte)Command.MoveForward;
 
-                                                    if (radius > 10.0f)
-                                                    {
-                                                        Cv2.Circle(imgMatrix, center, (int)radius, new Scalar(0, 255, 255), thickness: 2);
-                                                        Cv2.Circle(imgMatrix, center, 5, new Scalar(0, 0, 255), thickness: -1);
+														byte[] message = { command };
+														serial.Write(message, 0, 1);
 
-                                                        //find the ball which region on the screen horizontally [0-5]
-                                                        int xRegion = center.X.Map(0, width, 0, 3);
-                                                        //find the ball which region on the screen vertically [0-5]
-                                                        int yRegion = center.Y.Map(0, height, 0, 3);
-                                                        //find the ball is too far from camera or too close
-                                                        int zRegion = ((int)radius).Map(10, 400, 0, 3);
+														#endregion
+													}
+													else
+													{
+														Console.WriteLine($"{lastSeen.ToString()}");
+													}
+												}
 
-                                                        lastSeen = FindBallCoordinates((int)radius, center, new Point(width / 2, height / 2));
-
-                                                        #region Automatic Decisions
-
-                                                        byte command = 0;
-
-                                                        if (xRegion < 1 && yRegion < 1)
-                                                            command |= (byte)(Command.CamLeft | Command.CamUp);
-
-                                                        else if (xRegion < 1 && yRegion > 1)
-                                                            command |= (byte)(Command.CamLeft | Command.CamDown);
-
-                                                        else if (xRegion > 1 && yRegion < 1)
-                                                            command |= (byte)(Command.CamRight | Command.CamUp);
-
-                                                        else if (xRegion > 1 && yRegion > 1)
-                                                            command |= (byte)(Command.CamRight | Command.CamDown);
-
-                                                        else if (xRegion < 1)
-                                                            command |= (byte)Command.CamLeft;
-
-                                                        else if (xRegion > 1)
-                                                            command |= (byte)Command.CamRight;
-
-                                                        else if (yRegion < 1)
-                                                            command |= (byte)Command.CamUp;
-
-                                                        else if (yRegion > 1)
-                                                            command |= (byte)Command.CamDown;
-
-                                                        if (zRegion > 1)
-                                                            command |= (byte)Command.MoveBackward;
-
-                                                        else if (zRegion < 1)
-                                                            command |= (byte)Command.MoveForward;
-
-
-
-                                                        byte[] message = { command };
-                                                        serial.Write(message, 0, 1);
-
-                                                        #endregion
-
-                                                    }
-                                                    else
-                                                    {
-                                                        Console.WriteLine($"{lastSeen.ToString()}");
-                                                    }
-
-
-                                                }
-                                                
-
-                                                Cv2.ImEncode(".jpg", imgMatrix, out byte[] result, encoderParam);
+												Cv2.ImEncode(".jpg", imgMatrix, out byte[] result, encoderParam);
 												writer.Write(result.Length);
 												stream.Write(result, 0, result.Length);
-												//Console.Write($"\r{result.Length}        ");
 											}
 										}
 									}
 									finally
 									{
-										Console.Error.WriteLine();
+										Console.WriteLine();
 									}
 								}
 							}
@@ -202,26 +178,42 @@ namespace ObjTracker
 			}
 			catch (EndOfStreamException ex)
 			{
-				Console.Error.WriteLine(ex.Message);
+				Console.WriteLine(ex.Message);
 				goto re;
 			}
 			catch (IOException ex)
 			{
-				Console.Error.WriteLine(ex.Message);
+				Console.WriteLine(ex.Message);
 				goto re;
 			}
 			catch (Exception ex)
 			{
-				Console.Error.WriteLine(ex);
+				Console.WriteLine(ex);
 				return 1;
 			}
 			finally
 			{
 				listener.Stop();
-				Console.Error.WriteLine("\n");
+				Console.WriteLine("\n");
 			}
 			//goto re;
 			return 0;
+		}
+
+		static int Map(this int x, int in_min, int in_max, int out_min, int out_max)
+		{
+			return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+		}
+
+		static Scalar FindBallCoordinates(int ballRadiusInPixel, Point centerOfBall, Point centerOfScreen, float ballDiameter = 6.5f)
+		{
+			//finding ball distance in centimeters from camera with precalculated constants
+			double ballDist = 60 * 20 / (ballRadiusInPixel - 1);
+			//relative coordinates according to camera
+			double x = (centerOfBall.X - centerOfScreen.X) * ballDiameter / (ballRadiusInPixel - 1) / 2;
+			double y = (centerOfBall.Y - centerOfScreen.Y) * ballDiameter / (ballRadiusInPixel - 1) / 2;
+			double z = Math.Sqrt((Math.Pow(ballDist, 2) - Math.Pow(x, 2))) * 2;
+			return new Scalar(x, y, z);
 		}
 	}
 }
